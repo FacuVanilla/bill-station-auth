96a590c7f0826227b989add04b6555252e5393b0
Seun <101073504+FacuVanilla@users.noreply.github.com>
2025-09-01 12:12:46 +0100

    Fix static files: Force collection in Procfile and add static finders

--- auth_service/settings.py
+++ auth_service/settings.py
@@ -246,6 +246,12 @@ if DEBUG:
 else:
     STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
+# Ensure static files are found
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- auth_service/urls.py
+++ auth_service/urls.py
@@ -59,6 +59,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    patterns=[path('api/v1/', include('users.urls'))],
 )
 
 urlpatterns = [
